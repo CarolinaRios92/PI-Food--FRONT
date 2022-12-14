7384ff2b87a9918e7ad312895198acf85b3fa71b
unknown <caritu_rios@hotmail.com>
2022-12-14 11:03:05 -0300

    deploy

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./components/Home/Home";
 import Details from "./components/Details/Details";
 import RecipeCreate from "./components/RecipeCreate/RecipeCreate";
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:3001/";
+axios.defaults.baseURL = "https://pi-food-back-production-082f.up.railway.app/";
 
 function App() {
   return (
